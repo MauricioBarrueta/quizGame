e3511fe945116a1f7d26576b55c7752641180575
Mauricio <mauba22@outlook.com>
2026-09-08 14:42:47 -0600

    Se agregó link al quiz Guess The Song

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -18,7 +18,7 @@ export class MainComponent {
   /* Se mandan y asignan los valores al objeto y se muestra el Modal */
   showModal() {
     this.confirmationModal.showModal({
-      icon: '<img width="48" height="48" class="mx-auto drop-3d" alt="" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Google_Translate_logo.svg/512px-Google_Translate_logo.svg.png?20210606111727">',
+      icon: '<img width="48" height="48" class="mx-auto drop-3d" alt="" src="/google-translate.svg">',      
       title: '¿Quieres traducir las preguntas al español?',
       subtitle: 'Ten en cuenta que la traducción puede no ser muy precisa',
       confirmText: 'Traducir preguntas',
